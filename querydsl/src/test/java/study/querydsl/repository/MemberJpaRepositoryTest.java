e219fcec8ad489b8c5f15e8d5448c55eeea08ea1
Hosung <an3735297@pusan.ac.kr>
2023-08-10 14:23:53 +0900

    실무 활용 : 동적 쿼리 성능 최적화 - Builder

---
+++ querydsl/src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,87 @@
+package study.querydsl.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.entitiy.Member;
+import study.querydsl.entitiy.Team;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberJpaRepositoryTest {
+
+    @PersistenceContext
+    EntityManager em;
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void CRUD() {
+        Member member = new Member("member1", 10);
+        memberJpaRepository.save(member);
+
+        Member findMember = memberJpaRepository.findById(member.getId()).get();
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+
+        List<Member> all = memberJpaRepository.findAll();
+        assertThat(all).containsExactly(member);
+
+        List<Member> byUsername = memberJpaRepository.findByUsername(member.getUsername());
+        assertThat(byUsername).containsExactly(member);
+    }
+
+    @Test
+    public void CRUD_Querydsl() {
+        Member member = new Member("member1", 10);
+        memberJpaRepository.save(member);
+
+        Member findMember = memberJpaRepository.findById(member.getId()).get();
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+
+        List<Member> all = memberJpaRepository.findAll_Querydsl();
+        assertThat(all).containsExactly(member);
+
+        List<Member> byUsername = memberJpaRepository.findByUsername_Querydsl(member.getUsername());
+        assertThat(byUsername).containsExactly(member);
+    }
+
+    @Test
+    public void searchTest() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setUsername("member1");
+        condition.setTeamName("teamA");
+        condition.setAgeLoe(20);
+
+        List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+        assertThat(result).extracting("username").containsExactly("member1");
+    }
+}
